424c8de625e3e533ab3d778cc266f74dbc20b35f
Shiyu (Baran) Liu <liushiyu1994@gmail.com>
2019-07-13 18:55:07 -0400

    Change function name. Add README.md and licence.

--- README.md
+++ README.md
@@ -1,31 +1,44 @@
-# Heading 1
+# Reader Benchmark for Large CSV Files
 
 
+Pandas is a common toolkit for CSV readers. However, its performance is usually not satisfactory for large files. This project tests several different ways to find the best processing protocol for large CSV files.
 
-First paragraph.
 
-This is a [link](https://archive.ics.uci.edu/ml/datasets/human+activity+recognition+using+smartphones).
+## Requirements
 
-This is the second paragraph
+`numpy` and `pandas` packages are required. The script is tested on `python 3.6` with `numpy=1.16.2` and `pandas=0.23.3`.
 
-This is an ordered list:
-1. This is the first item.
-2. This is the second item.
+## Get started
 
-This is a figure
-
-![Loss function values](./figures/Loss.png "Loss function values")
-
-This is a script inline `main.py`.
-
-This is a code example
+Just simply run: 
 
 ```
-Give examples
+python src/parallel_reader.py
 ```
 
+First, a test data set, CSV file containing random float numbers is generated. Then, several methods are utilized to extract four columns in the data set, and output to a new CSV file. Running time of different methods will be measured and displayed. The methods include:
+
+1. Ordinary Pandas I/O method.
+2. Pandas I/O method with multi-thread acceleration. Writing part is in the main thread.
+3. Pandas I/O method with multi-process acceleration. Writing part is in the main process.
+4. Pandas I/O method with multi-process acceleration. Writing part is in the individual process.
+5. Normal for-loop method.
+6. For-loop method with multi-thread acceleration. Writing part is in the main thread.
+7. For-loop method with multi-process acceleration. Writing part is in the main process.
+
+## Conclusions
+
++ For-loop based method is usually much faster than those with Pandas, could be up to 10 times in very large CSV file.
++ In methods using for-loop, thread-based parallel is slightly better than process-based parallel, which are both significantly better than single process in very large CSV file.
++ Process-based for-loop parallel may be better than thread-based parallel in data processing with more complicated computation. 
++ Writing function cannot be parallel, or the order of final files will be different.
+
+
+## Authors
 
 + **Shiyu Liu** - *Initial work* - [liushiyu1994](https://github.com/PurpleBooth)
 
+
+## License
 
 This software is released under the [MIT License](LICENSE-MIT).
--- src/parallel_reader.py
+++ src/parallel_reader.py
@@ -1,10 +1,11 @@
 import time
 import os
+import multiprocessing as mp
+import multiprocessing.dummy as mt
+import warnings
 
 import pandas as pd
 import numpy as np
-import multiprocessing as mp
-import multiprocessing.dummy as mt
 
 target_col_list = [1, 5, 10, 20]
 thread_num = os.cpu_count()
@@ -19,6 +20,16 @@ else:
 raw_csv_file = './data/large_csv.csv'
 processed_csv_file = './data/processed_csv.csv'
 
+name_dict = {
+    'single_processor': "Pandas in single processor",
+    'thread_parallel_processor': "Pandas with thread-based parallel",
+    'process_parallel_processor': "Pandas with process-based parallel",
+    'thread_parallel_with_writer_processor': "Pandas with thread-based parallel writer",
+    'for_loop_single_processor': "for-loop in single processor",
+    'for_loop_thread_parallel_processor': "for-loop with thread-based parallel",
+    'for_loop_process_parallel_processor': "for-loop with process-based parallel",
+}
+
 
 def large_csv_generator():
     random_num = np.random.random([n_row, n_col])
@@ -27,13 +38,16 @@ def large_csv_generator():
 
 def clock_and_check(func, rate=0.5):
     def clocked_and_checked(*args, **kwargs):
+        raw_name = func.__name__
+        user_friendly_name = name_dict[raw_name]
+        print("Start processing by {}...".format(user_friendly_name))
         t0 = time.perf_counter()
         result = func(*args, **kwargs)
         elapsed = time.perf_counter() - t0
-        name = func.__name__
-        arg_str = ', '.join(repr(arg) for arg in args)
-        print('[%0.8fs] %s(%s)' % (elapsed, name, arg_str))
-        check_result(rate)
+        print("Finish processing by {}.".format(user_friendly_name))
+        # arg_str = ', '.join(repr(arg) for arg in args)
+        print('[%0.8fs] %s' % (elapsed, user_friendly_name))
+        check_result(rate, user_friendly_name)
         return result
     return clocked_and_checked
 
@@ -47,7 +61,7 @@ def single_pandas_with_appending(df, f_out):
     new_df.to_csv(f_out, header=None, index=False)
 
 
-def check_result(rate):
+def check_result(rate, function_name):
     with open(raw_csv_file) as f1_in, open(processed_csv_file) as f2_in:
         for line1, line2 in zip(f1_in, f2_in):
             if np.random.random() < rate:
@@ -58,16 +72,17 @@ def check_result(rate):
                 num1 = float(l1[raw_col_index])
                 num2 = float(l2[new_col_index])
                 if abs(num1 - num2) > 1e-10:
-                    raise ValueError("Not equal!\nNum1: {}\nNum2: {}".format(num1, num2))
+                    warnings.warn("\n{}:\nNot equal! Num1: {} Num2: {}".format(function_name, num1, num2))
+                    return
 
         if f1_in.readline() != f2_in.readline():
-            raise ValueError("EOF is not equal!")
+            raise warnings.warn("\n{}:\nEOF is not equal!".format(function_name))
+            return
     print("Test passed!")
 
 
 @clock_and_check
 def single_processor():
-    print("Start processing by Pandas...")
     df_iter = pd.read_csv(raw_csv_file, header=None, chunksize=chunksize)
     with open(processed_csv_file, 'w') as f_out:
         pass
@@ -75,12 +90,10 @@ def single_processor():
         for input_df in df_iter:
             new_df = single_pandas_func(input_df)
             new_df.to_csv(f_out, header=None, index=False)
-    print("Finish processing by Pandas.")
 
 
 @clock_and_check
 def thread_parallel_processor():
-    print("Start processing by thread-based parallel...")
     df_iter = pd.read_csv(raw_csv_file, header=None, chunksize=chunksize)
     with open(processed_csv_file, 'w') as f_out:
         pass
@@ -89,12 +102,10 @@ def thread_parallel_processor():
             result_iter = pool.imap(single_pandas_func, df_iter)
             for result_df in result_iter:
                 result_df.to_csv(f_out, header=None, index=False)
-    print("Finish processing by thread-based parallel.")
 
 
 @clock_and_check
 def process_parallel_processor():
-    print("Start processing by process-based parallel...")
     df_iter = pd.read_csv(raw_csv_file, header=None, chunksize=chunksize)
     with open(processed_csv_file, 'w') as f_out:
         pass
@@ -103,19 +114,16 @@ def process_parallel_processor():
             result_iter = pool.imap(single_pandas_func, df_iter)
             for result_df in result_iter:
                 result_df.to_csv(f_out, header=None, index=False)
-    print("Finish processing by process-based parallel.")
 
 
 @clock_and_check
 def thread_parallel_with_writer_processor():
-    print("Start processing by thread-based parallel with writer...")
     df_iter = pd.read_csv(raw_csv_file, header=None, chunksize=chunksize)
     with open(processed_csv_file, 'w') as f_out:
         pass
     with open(processed_csv_file, 'a') as f_out:
         with mt.Pool(thread_num) as pool:
-            pool.map_async(lambda x: single_pandas_with_appending(x, f_out), df_iter)
-    print("Finish processing by thread-based parallel with writer.")
+            pool.map(lambda x: single_pandas_with_appending(x, f_out), df_iter)
 
 
 def single_line_func(line):
@@ -155,12 +163,12 @@ def for_loop_process_parallel_processor():
 
 def main():
     print("Random number generating")
-    # large_csv_generator()
+    large_csv_generator()
     print("Random number generated!")
     single_processor()
-    # thread_parallel_processor()
-    # process_parallel_processor()
-    # thread_parallel_with_writer_processor()
+    thread_parallel_processor()
+    process_parallel_processor()
+    thread_parallel_with_writer_processor()
     for_loop_single_processor()
     for_loop_thread_parallel_processor()
     for_loop_process_parallel_processor()
